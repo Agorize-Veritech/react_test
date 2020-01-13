338e52b3654e4cecf3449b7223b97fe27e42c409
Longxi Lin <53550033+longxilin@users.noreply.github.com>
2020-01-12 20:31:30 -0500

    upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+var percentage = 66;
+var test = 'hello';
+
+class Keyword extends React.Component{
+  render(){
+    return(
+      <CircularProgressbar value={percentage} text={`${test}`} />
+    );
+  }
+}
 
 function App() {
+  const percentage = 66;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header class="App-header"><p>title</p>
+      <Container>
+        <Row>
+          <Col md={4}><p>hello world!</p></Col>
+          <Col md={8}>
+            <Row>
+              <Col>
+                <Keyword />
+              </Col>
+              <Col>
+                <Keyword />
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+    </Container>
+    </header>
     </div>
   );
 }
